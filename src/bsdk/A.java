aae6efbd07225276eba7dc7c94bf4b9539ac2440
atulluta <atulgithub@gmail.com>
2018-09-29 10:49:21 +0530

    aa

--- src/bsdk/A.java
+++ src/bsdk/A.java
@@ -4,9 +4,9 @@ public class A {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("2");
-		System.out.println("3");
-		System.out.println("4");
+		System.out.println("2.2");
+		System.out.println("3.3");
+		System.out.println("4.4");
 	}
 
 }
