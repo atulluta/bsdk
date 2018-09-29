ed1af3351bd1fee9bc670726c3ec72689d9eccce
atulluta <42914856+atulluta@users.noreply.github.com>
2018-09-29 10:48:22 +0530

    Update A.java

--- src/bsdk/A.java
+++ src/bsdk/A.java
@@ -4,9 +4,9 @@ public class A {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("2");
-		System.out.println("3");
-		System.out.println("4");
+		System.out.println("2.1");
+		System.out.println("3.1");
+		System.out.println("4.1");
 	}
 
 }
